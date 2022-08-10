2b49119459157e6eedd63a6ad2b4edd51fc3c304
ssohy <sohyeonj415@gmail.com>
2022-08-10 15:31:13 +0900

    git commit

---
+++ app/src/main/java/com/example/project2/_3_T_StationSchoolActivity.java
@@ -0,0 +1,14 @@
+package com.example.project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class _3_T_StationSchoolActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3_tstation_school);
+    }
+}
