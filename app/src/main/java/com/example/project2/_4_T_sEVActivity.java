1a63b9b030da63b71e8ac781352e4fbef78bdfd5
ssohy <sohyeonj415@gmail.com>
2022-08-13 17:02:18 +0900

    git commit

--- app/src/main/java/com/example/project2/_4_T_sEVActivity.java
+++ app/src/main/java/com/example/project2/_4_T_sEVActivity.java
@@ -3,8 +3,10 @@ package com.example.project2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class _4_T_sEVActivity extends AppCompatActivity {
+    Button Giheung, KangnamUniv, Jiseok, Eojeong, Dongbaek, Chodang	, Samga	, CityHallYonginUniv, MyongjiUniv, Gimnyangjang, StadiumSongdamCollege, Gojin, Bopyeong, Dunjeon, JeondaeEverland;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
