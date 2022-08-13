fe79af7cf4bcd5dbaa415b361b8d4d0355892c96
Sena-Han <hsysn1@naver.com>
2022-08-13 22:07:14 +0900

    commit

--- app/src/main/java/com/example/project2/_4_T_sEVActivity.java
+++ app/src/main/java/com/example/project2/_4_T_sEVActivity.java
@@ -2,11 +2,13 @@ package com.example.project2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
 
 public class _4_T_sEVActivity extends AppCompatActivity {
-    Button Giheung, KangnamUniv, Jiseok, Eojeong, Dongbaek, Chodang	, Samga	, CityHallYonginUniv, MyongjiUniv, Gimnyangjang, StadiumSongdamCollege, Gojin, Bopyeong, Dunjeon, JeondaeEverland;
+    TextView Giheung, KangnamUniv, Jiseok, Eojeong, Dongbaek, Chodang	, Samga	, CityHallYonginUniv, MyongjiUniv, Gimnyangjang, StadiumSongdamCollege, Gojin, Bopyeong, Dunjeon, JeondaeEverland;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
