c1ee5739b7d4e1fb6a5a866bc5fd8beb59bf6daa
Sena-Han <hsysn1@naver.com>
2022-08-15 12:10:21 +0900

    commit

---
+++ app/src/main/java/com/example/project2/_s1_gsp.java
@@ -0,0 +1,15 @@
+package com.example.project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class _s1_gsp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_4_ts1_gsp);
+
+    }
+}
